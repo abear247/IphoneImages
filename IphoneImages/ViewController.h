0bcb9e62d54b337f71057643736798529a140d5e
alexbearinger <alexbearinger@Alexs-MacBook-Pro.local>
2017-01-30 11:34:37 -0500

    first commit

---
+++ IphoneImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IphoneImages
+//
+//  Created by Alex Bearinger on 2017-01-30.
+//  Copyright © 2017 Alex Bearinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IphoneImages/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  IphoneImages
+//
+//  Created by Alex Bearinger on 2017-01-30.
+//  Copyright © 2017 Alex Bearinger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSURL *url = [NSURL URLWithString:@"http://i.imgur.com/bktnImE.png"];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
