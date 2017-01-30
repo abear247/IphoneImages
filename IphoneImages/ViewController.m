be9cf6710a3df097d1c8481f542803b44674e372
alexbearinger <alexbearinger@Alexs-MacBook-Pro.local>
2017-01-30 12:10:20 -0500

    completed

--- IphoneImages/ViewController.m
+++ IphoneImages/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *iPhoneImageView;
 
 @end
 
@@ -17,7 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
     NSURL *url = [NSURL URLWithString:@"http://i.imgur.com/bktnImE.png"];
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:url completionHandler:^(NSURL *_Nullable location, NSURLResponse *_Nullable response, NSError *_Nullable error){
+        if(error){
+            NSLog(@"error: %@",error.localizedDescription);
+            return;
+        }
+        NSData *data = [NSData dataWithContentsOfURL:location];
+        UIImage *image = [UIImage imageWithData:data];
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            self.iPhoneImageView.image = image;
+        }];
+        }];
+    [downloadTask resume];
 }
 
 
